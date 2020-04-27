ce1463eb872e3b426c0513d6f910ffb063c882ec
Felipe Rocha <64161583+Felipe-rocha@users.noreply.github.com>
2020-04-26 23:43:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,15 +7,22 @@ const overlay = document.querySelector('.overlay');
 /* Looping through images */
 
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
+//newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
+
+var imageButton = document.querySelector(".dark");
 
 imageButton.addEventListener("click", function(){
     if (imageButton.textContent == "Darken"){
         imageButton.textContent = "Lighten";
+        var escureceImagem = document.querySelector(".displayed-img");
+        escureceImagem.classList.add("escurece-img");
+
     }else{
         if (imageButton.textContent == "Lighten"){
             imageButton.textContent = "Darken";
+            var escureceImagem = document.querySelector(".displayed-img");
+            escureceImagem.classList.remove("escurece-img");
         }
     };
 });
